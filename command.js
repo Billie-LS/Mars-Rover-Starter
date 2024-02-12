2452d4be42a4fbde0e093c82513553d8bfe4b624
Billie-LS <lskylizard@yahoo.com>
2024-02-12 17:08:57 -0600

    class Command - all still fail

--- command.js
+++ command.js
@@ -1,12 +1,20 @@
+/*
+commandType is a string that represents the type of command. 
+note, command type will be either- 
+MODE_CHANGE, 
+MOVE, or 
+STATUS_CHECK.
+value is a value related to the type of command
+*/
+// class builds object with two properties: constructor(commandType, value)
 class Command {
-   constructor(commandType, value) {
-     this.commandType = commandType;
-     if (!commandType) {
-       throw Error("Command type required.");
-     }
-     this.value = value;
-   }
- 
- }
- 
- module.exports = Command;
+  constructor(commandType, value) {
+    this.commandType = commandType;
+    if (!commandType) {
+      throw Error("Command type required.");
+    }
+    this.value = value;
+  }
+}
+
+module.exports = Command;
