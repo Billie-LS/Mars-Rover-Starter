d30bf4a5d6caac88f9dc2d78b6dda42179770d46
Billie-LS <lskylizard@yahoo.com>
2024-02-12 23:32:19 -0600

    message test/spec and module

--- message.js
+++ message.js
@@ -1,5 +1,28 @@
+/*
+Message Class Description
+class builds object with two properties: constructor(name, commands)
+ - name is a string that is the name of the message. 
+ - commands is an array of Command objects.
+*/
 class Message {
-   // Write code here!
+  constructor(name, commands) {
+    this.name = name;
+    if (!name) {
+      throw Error("Message name required.");
+    }
+    this.commands = commands;
+  }
+  // Write code here!
 }
 
 module.exports = Message;
+
+// class Command {
+//    constructor(commandType, value) {
+//      this.commandType = commandType;
+//      if (!commandType) {
+//        throw Error("Command type required.");
+//      }
+//      this.value = value;
+//    }
+//  }
