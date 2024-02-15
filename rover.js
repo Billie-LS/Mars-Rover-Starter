46db3bfc22cb3a7863c2d5af93ac09ba02e98c2e
Billie-LS <lskylizard@yahoo.com>
2024-02-14 21:53:09 -0600

    Rover constructor and unit testing 7, 8

--- rover.js
+++ rover.js
@@ -1,5 +1,44 @@
+/*
+Rover
+Rover receives a message object, 
+updates its properties from the message, and 
+returns the results.
+class builds rover object with properties, and 
+also contains function outside constructor to handle updates to properties.
+
+constructor(position)
+
+position is a number representing the rover’s position.
+Sets this.position to position
+Sets this.mode to 'NORMAL'
+Sets the default value for generatorWatts to 110
+receiveMessage(message)
+
+message is a Message object
+Returns an object containing at least two properties:
+message: the name of the original Message object
+results: an array of results. Each element in the array is an object that corresponds to one Command in message.commands.
+Updates certain properties of the rover object
+*/
+
 class Rover {
-   // Write code here!
+  constructor(position, mode = "NORMAL", generatorWatts = 110) {
+    this.position = position;
+    this.mode = mode;
+    this.generatorWatts = generatorWatts;
+  }
+  receiveMessage(message) {
+    let response = {
+      message: message.name,
+      results: [],
+    };
+    return response;
+  }
 }
 
 module.exports = Rover;
+
+/*
+ “constructor sets position and default values for 
+ mode and generatorWatts”.
+ */
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -1,13 +1,33 @@
-const Rover = require('../rover.js');
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Rover = require("../rover.js");
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
+describe("Rover class", function () {
+  // test 7
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    let rover = new Rover(9832);
+    expect(rover.position).toEqual(9832);
+    expect(rover.mode).toBe("NORMAL");
+    expect(rover.generatorWatts).toEqual(110);
+  });
 
-describe("Rover class", function() {
-
-  // 7 tests here!
-
+  // test 8
+  it("response returned by receiveMessage contains the name of the message", function () {
+    let commands = [
+      new Command("MODE_CHANGE", "LOW_POWER"),
+      new Command("STATUS_CHECK"),
+    ];
+    let message = new Message("Test message with two commands", commands);
+    let rover = new Rover(9832);
+    let response = rover.receiveMessage(message);
+    expect(response.message).toBe("Test message with two commands");
+  });
 });
+
+/*
+“constructor sets position and default values for 
+mode and generatorWatts”.
+*/
