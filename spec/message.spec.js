6ff206c00e341076e5077028ce52a14dc88f223c
Billie-LS <lskylizard@yahoo.com>
2024-02-17 20:37:28 -0600

    rover - class Message Unit Testing

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -4,14 +4,19 @@ const Command = require("../command.js");
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function () {
+describe("Message class", () => {
   // Test 4
-  // TODO: description here
-  it("throws error if a name is NOT passed into the constructor as the first parameter", function () {
-    expect(function () {
-      new Message();
-    }).toThrow(new Error("Message name required."));
+  it("throws error if a name is NOT passed into the constructor as the first parameter", () => {
+    expect(() => new Message()).toThrow(new Error("Message name required."));
   });
+
+  // // Test 4
+  // it("throws error if a name is NOT passed into the constructor as the first parameter", () => {
+  //   expect(() => {
+  //     new Message();
+  //   }).toThrow(new Error("Message name required."));
+  // });
+
   // Test 5
   // TODO: description here
   it("constructor sets name", function () {
