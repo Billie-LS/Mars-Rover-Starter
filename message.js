b52330243fd66ffcfcb24321e6f3475d9f2351e7
Billie-LS <lskylizard@yahoo.com>
2024-02-19 13:02:29 -0600

    completed Rover class, Roverspec, passed all studentgrade.spec

--- message.js
+++ message.js
@@ -1,11 +1,12 @@
 /*
 Message Class Description
-class builds object with two properties: constructor(name, commands)
- - name is a string that is the name of the message. 
- - commands is an array of Command objects.
+Message class builds object with two properties:
+constructor(name, commands)
+name is a string that is the name of the message.
+commands is an array of Command objects.
 */
 class Message {
-  constructor(name, commands) {
+  constructor(name, commands = []) {
     if (!name) throw Error("Message name required.");
     this.name = name;
     this.commands = commands;
@@ -23,13 +24,3 @@ class Message {
 // }
 
 module.exports = Message;
-
-// class Command {
-//    constructor(commandType, value) {
-//      this.commandType = commandType;
-//      if (!commandType) {
-//        throw Error("Command type required.");
-//      }
-//      this.value = value;
-//    }
-//  }
