7711d67ab76f9de74d593e7c34ee84ad1200a6c2
Billie-LS <lskylizard@yahoo.com>
2024-02-20 04:51:08 -0600

    commented command.spec; cleaning sandbox

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,48 +1,39 @@
+// import Command class from command.js
 const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+// do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+
+// describe Command class testing
 describe("Command class", function () {
   // Test 1
+  // test error thrown if commandType not passed in constructor as first parameter
   it("throws error if command type is NOT passed into constructor as the first parameter", () => {
+    // confirm constructing Command object without commandType throws an error
     expect(() => new Command()).toThrow(new Error("Command type required."));
   });
 
-  // // Test 1
-  // it("throws error if command type is NOT passed into constructor as the first parameter", () => {
-  //   expect(() => {
-  //     new Command();
-  //   }).toThrow(new Error("Command type required."));
-  // });
-
   // Test 2
+  // confirm constructor sets commandType property correctly
   it("constructor sets command type", () => {
+    // define a test commandType
     let testType = "Move";
+    // generate Command object with test commandType
     let command = new Command(testType);
+    // confirm commandType property set correctly
     expect(command.commandType).toBe(testType);
   });
 
-  // // Test 2
-  // it("constructor sets command type", () => {
-  //   let command = new Command("Move");
-  //   expect(command.commandType).toBe("Move");
-  // });
-
-  // it("constructor sets command type", function() {
-  //   expect( function() { new Command();}).toThrow(new Error('Command type required.'));
-  // });
-
   // Test 3
+  // confirm constructor sets value passed in as the 2nd argument
   it("constructor sets a value passed in as the 2nd argument", () => {
+    // define a test commandType
     let testType = "Move";
+    // define a test value
     let testValue = 12000;
+    // generate Command object with test commandType and test value
     let command = new Command(testType, testValue);
+    // confirm value property set correctly
     expect(command.value).toEqual(testValue);
   });
-
-  // // Test 3
-  // it("constructor sets a value passed in as the 2nd argument", () => {
-  //   let command = new Command("Move", 12000);
-  //   expect(command.value).toEqual(12000);
-  // });
 });
