5825c4db7b1cc94bf408cd0a43c58f754646f2e8
Billie-LS <lskylizard@yahoo.com>
2024-02-20 04:46:36 -0600

    commented rover.js; cleaning sandbox

--- rover.js
+++ rover.js
@@ -20,7 +20,6 @@ message: the name of the original Message object
 results: an array of results. Each element in the array is an object that corresponds to one Command in message.commands.
 Updates certain properties of the rover object
 */
-
 class Rover {
   constructor(position) {
     // constructor sets position to provided argument; parse to number
@@ -77,79 +76,10 @@ class Rover {
       }
     }
 
-    // // method receiveMessage
-    // receiveMessage(message) {
-    //   // create response object with values message.name and empty array for results
-    //   let response = {
-    //     message: message.name,
-    //     results: [],
-    //   };
-
-    //   // iterate each command in message
-    //   for (let command of message.commands) {
-    //     // logic check if commandType is MODE_CHANGE
-    //     if (command.commandType === "MODE_CHANGE") {
-    //       // Respond to MODE_CHANGE command
-    //       this.mode = command.value; // Update rover's mode to the new value specified in the command
-    //       response.results.push({ completed: true }); // Push a completed result object
-
-    //       // logic check if commandType is STATUS_CHECK
-    //     } else if (command.commandType === "STATUS_CHECK") {
-    //       // Respond to STATUS_CHECK command
-    //       response.results.push({
-    //         // Push result object with rover status
-    //         completed: true,
-    //         roverStatus: {
-    //           mode: this.mode,
-    //           generatorWatts: this.generatorWatts,
-    //           position: this.position,
-    //         },
-    //       });
-    //     } else {
-    //       // For other commands, push an empty result object
-    //       response.results.push({});
-    //     }
-    //   }
-
-    // // method receiveMessage
-    // receiveMessage(message) {
-    //   // create response object with values message.name and empty array for results
-    //   let response = {
-    //     message: message.name,
-    //     results: [],
-    //   };
-
-    //   // iterate each command in message
-    //   for (let command of message.commands) {
-    //     // logic check if commandType is STATUS_CHECK
-    //     if (command.commandType === "STATUS_CHECK") {
-    //       // respond to STATUS_CHECK command push result object with rover status
-    //       response.results.push({
-    //         completed: true,
-    //         roverStatus: {
-    //           mode: this.mode,
-    //           generatorWatts: this.generatorWatts,
-    //           position: this.position,
-    //         },
-    //       });
-    //     } else {
-    //       // For other commands, push an empty result object
-    //       response.results.push({});
-    //     }
-    //   }
-
-    // receiveMessage(message) {
-    //   let response = {
-    //     message: message.name,
-    //     results: [],
-    //   };
-    //   for (let command of message.commands) {
-    //     response.results.push({});
-    //   }
-
     // return response object with executed commands and results
     return response;
   }
 }
 
+// export Rover class as Rover module
 module.exports = Rover;
