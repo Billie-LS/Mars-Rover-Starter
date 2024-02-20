e6191cb12a098c22b942750ebb6a7c903eff8717
Billie-LS <lskylizard@yahoo.com>
2024-02-20 04:54:02 -0600

    commented message.spec; cleaning sandbox

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,4 +1,6 @@
+// import Message class from message.js
 const Message = require("../message.js");
+// import Command class from command.js
 const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
@@ -10,52 +12,37 @@ constructor(name, commands)
 name is a string that is the name of the message.
 commands is an array of Command objects.
 */
+// describe Message class testing
 describe("Message class", () => {
   // Test 4
+  // test error thrown if name not passed in constructor as first parameter
   it("throws error if a name is NOT passed into the constructor as the first parameter", () => {
+    // confirm constructing  Message object without name throws error
     expect(() => new Message()).toThrow(new Error("Message name required."));
   });
 
-  // // Test 4
-  // it("throws error if a name is NOT passed into the constructor as the first parameter", () => {
-  //   expect(() => {
-  //     new Message();
-  //   }).toThrow(new Error("Message name required."));
-  // });
-
   // Test 5
+  // confirm constructor sets name property correctly
   it("constructor sets name", () => {
+    // define a test name
     let testName = "Test message with two commands";
+    // generate Message object with test name
     let message = new Message(testName);
+    // confirm name property set correctly
     expect(message.name).toBe(testName);
   });
 
-  // // Test 5
-  // it("constructor sets name", () => {
-  //   let message = new Message("Test message with two commands");
-  //   expect(message.name).toBe("Test message with two commands");
-  // });
-
   // Test 6
+  // confirm constructor sets commands array property correctly
   it("contains a commands array passed into the constructor as the 2nd argument", () => {
+    // define array of commands
     let commands = [
       new Command("MODE_CHANGE", "LOW_POWER"),
       new Command("STATUS_CHECK"),
     ];
+    // generate Message object with test name and test commands
     let message = new Message("Test message with two commands", commands);
+    // confirm commands array property set correctly
     expect(message.commands).toEqual(commands);
   });
-
-  // // Test 6
-  // it("contains a commands array passed into the constructor as the 2nd argument", () => {
-  //   let commands = [
-  //     new Command("MODE_CHANGE", "LOW_POWER"),
-  //     new Command("STATUS_CHECK"),
-  //   ];
-  //   let message = new Message("Test message with two commands", commands);
-  //   expect(message.commands).toEqual([
-  //     new Command("MODE_CHANGE", "LOW_POWER"),
-  //     new Command("STATUS_CHECK"),
-  //   ]);
-  // });
 });
