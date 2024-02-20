8b01d341590c05100e5df0419fb1d94fc5d8bd8e
Billie-LS <lskylizard@yahoo.com>
2024-02-20 04:43:32 -0600

    commented command.js and message.js; cleaning sandbox

--- command.js
+++ command.js
@@ -1,28 +1,24 @@
 /*
-commandType is a string that represents the type of command. 
-note, command type will be either- 
-MODE_CHANGE, 
-MOVE, or 
-STATUS_CHECK.
-value is a value related to the type of command
+This class builds an object with two properties: 
+constructor(commandType, value)
+commandType is a string that represents the type of command:
+MODE_CHANGE, MOVE, or STATUS_CHECK.
+value is a value related to the type of command.
 */
-// class builds object with two properties: constructor(commandType, value)
+// class named Command
 class Command {
+  // constructor with two properties, commandType and value
   constructor(commandType, value) {
+    // logic check for commandType is input or throw an error
     if (!commandType) throw Error("Command type required.");
+
+    // constructor set commandType property to commandType argument provided
     this.commandType = commandType;
+
+    // constructor set value property to value argument provided
     this.value = value;
   }
 }
 
-// class Command {
-//   constructor(commandType, value) {
-//     this.commandType = commandType;
-//     if (!commandType) {
-//       throw Error("Command type required.");
-//     }
-//     this.value = value;
-//   }
-// }
-
+// export Command class as Command module
 module.exports = Command;
--- message.js
+++ message.js
@@ -5,22 +5,21 @@ constructor(name, commands)
 name is a string that is the name of the message.
 commands is an array of Command objects.
 */
+
+// class named Command
 class Message {
+  // constructor with two properties, name and commands, commands default to empty array
   constructor(name, commands = []) {
+    // logic check for name is input or throw an error
     if (!name) throw Error("Message name required.");
+
+    // constructor set name property to name argument provided
     this.name = name;
+
+    // constructor set commands property to commands argument provided
     this.commands = commands;
   }
 }
 
-// class Message {
-//   constructor(name, commands) {
-//     this.name = name;
-//     if (!name) {
-//       throw Error("Message name required.");
-//     }
-//     this.commands = commands;
-//   }
-// }
-
+// export Message class as Message module
 module.exports = Message;
